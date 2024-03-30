fb6108340b46823ae0f55355fb50ea728b57929c
vinicius silva <vimsilva@gmail.com>
2024-03-30 15:40:37 -0300

    Start project

---
+++ api-mongodb/Program.cs
@@ -0,0 +1,15 @@
+namespace api_mongodb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
